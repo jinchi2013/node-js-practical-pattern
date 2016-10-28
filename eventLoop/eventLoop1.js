3454fa62fd641c7f8bfbfd09f1958765e90974a9
Calvin Jin <Calvin.Jin.CFT@jcrew.com>
2016-10-28 10:39:59 -0400

    figure out the how event newListener work

--- eventLoop/eventLoop1.js
+++ eventLoop/eventLoop1.js
@@ -6,20 +6,26 @@ class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 /*******
 	Event: 'newListener'
+	The EventEmitter instance will emit its own 'newListener' event 
+	before a listener is added to its internal array of listeners.
 *******/
 //Only do once so we don't loop forever
+
 myEmitter.once('newListener', (event, listener)=> {
-	if(event === 'event') {
+	console.log(event);
+	console.log(listener);
+
+	if(event === 'event1') {
 		//Insert a new listener in front
-		myEmitter.on('event', ()=>{
+		myEmitter.on('event1', ()=>{
 			console.log('BBBBBB');
 		});
 	}
 });
 
-myEmitter.on('event', ()=> {
-	//callback for event, which will 
+myEmitter.on('event1', ()=> {
 	console.log('AAAAA');
 });
 
-myEmitter.emit('event');
+myEmitter.emit('event1');
+//This will trigger two listener for event1 event

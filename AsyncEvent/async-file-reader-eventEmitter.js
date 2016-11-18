656794c91e4aa4e6a11408f84ee6b6af4eccc250
Calvin Jin <Calvin.Jin.CFT@jcrew.com>
2016-11-18 15:58:42 -0500

    observable object pattern
    use util from core node to inherits from EventEmitter

---
+++ AsyncEvent/async-file-reader-eventEmitter.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const fs = require('fs');
+const EventEmitter = require('events');
+const util = require('util');
+
+class FindPattern extends EventEmitter {
+	constructor (regex) {
+		super();
+		this.regex = regex;
+		this.files = [];
+	}
+
+	addFile (file) {
+		this.files.push(file);
+		return this;
+	}
+
+	find () {
+		this.files.forEach( (file) => {
+			fs.readFile( file, 'utf8', (err, content)=>{
+				if(err){
+					this.emit('error', err);
+				}
+
+				this.emit('fileread', file);
+				let match = content.match(this.regex);
+				if(match) {
+					match.forEach((elem)=>{
+						this.emit('found', file, elem);
+					});
+				}
+			});
+		});
+
+		return this;
+	}
+}
+
+const findPatternObject = new FindPattern(/hello \w+/);
+
+findPatternObject
+	.addFile('fileA.txt')
+	.addFile('fileB.json')
+	.find()
+	.on('fileread', (file)=>{
+		console.log(`Reading the file ${file}`);
+	})
+	.on('found', (file, match)=>{
+		console.log(`Match ${match} in file ${file}`);
+	})
+	.on('error', (err)=>{
+		console.log(`Error emitted ${err.message}`);
+	});

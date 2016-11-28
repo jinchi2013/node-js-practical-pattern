711640e2681b616729b9c8177cfa05131245e591
Calvin Jin <jinchi2013us@gmail.com>
2016-11-27 22:39:08 -0500

    add getLinkUrl and getPageLinks method to utils class

--- async-control-flow-pattern/utils/utilities.js
+++ async-control-flow-pattern/utils/utilities.js
@@ -3,13 +3,18 @@
  */
 "use strict";
 const urlParse = require('url').parse;
+const urlFormat = require('url').format;
+const urlResolve = require('url').resolve;
 const slug = require('slug');
 const path = require('path');
+const cheerio = require('cheerio'); //jquery lib
 
-class urlToFilename {
-    constructor (url) {
-        this.parsedUrl = urlParse(url);
-        this.urlPath = urlParse(url).path.split('/')
+class utils {
+    constructor () {}
+
+    filename (url) {
+        let parsedUrl = urlParse(url);
+        let urlPath = urlParse(url).path.split('/')
             .filter((component)=>{
                 return component !== '';
             })
@@ -17,15 +22,32 @@ class urlToFilename {
                 return slug(component);
             })
             .join('/');
-    }
-
-    filename () {
-        let filename = path.join(this.parsedUrl.hostname, this.urlPath);
+        let filename = path.join(parsedUrl.hostname, urlPath);
         if(!path.extname(filename).match(/htm/)){
             filename +=".html";
         }
         return filename;
     }
+
+    getLinkUrl (currentUrl, element) {
+        let link = urlResolve(currentUrl, element.attribs.href || "");
+        let parsedLink = urlParse(link);
+        let currentParsedUrl = urlParse(currentUrl);
+        if(parsedLink.hostname !== currentParsedUrl.hostname || !parsedLink.pathname) {
+            return null;
+        }
+        return link;
+    }
+
+    getPageLinks (currentUrl, body) {
+        return [].slice.call(cheerio.load(body)('a'))
+            .map((element) => {
+                return this.getLinkUrl(currentUrl, element);
+            }).bind(this)
+            .filter((element) =>{
+                return !!element;
+            });
+    }
 }
 
-export default urlToFilename;
+export default utils;

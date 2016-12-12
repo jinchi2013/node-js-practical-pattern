3b899320de3e2a6c2110e5df1b97bd0885cafc8c
Calvin Jin <jinchi2013us@gmail.com>
2016-12-11 19:58:48 -0500

    remove var

--- async-control-flow-pattern/generators-patterns/clone-fs-generator-pattern.js
+++ async-control-flow-pattern/generators-patterns/clone-fs-generator-pattern.js
@@ -1,22 +1,23 @@
+"use strict";
 function asyncFlow(generatorFunction) {
     function callback(err) {
         if(err) {
             return generator.throw(err);
         }
-        var results = Array.prototype.slice.call(arguments, 1);
+        let results = Array.prototype.slice.call(arguments, 1);
         generator.next(results.length > 1 ? results : results[0]);
     }
 
-    var generator = generatorFunction(callback);
+    let generator = generatorFunction(callback);
     generator.next();
 }
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 asyncFlow(function* (callback) {
-    var fileName = path.basename(__filename);
-    var myself = yield fs.readFile(fileName, 'utf8', callback);
+    let fileName = path.basename(__filename);
+    let myself = yield fs.readFile(fileName, 'utf8', callback);
 
     yield fs.writeFile('clone_of_' + fileName, myself, callback);
     console.log('Clone Created!');

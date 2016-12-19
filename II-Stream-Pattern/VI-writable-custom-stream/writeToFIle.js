ea321fbc167994febbc26f6978e9ed3d16fd8a47
Calvin Jin <jinchi2013us@gmail.com>
2016-12-19 12:13:42 -0500

    syntax update

--- II-Stream-Pattern/VI-writable-custom-stream/writeToFIle.js
+++ II-Stream-Pattern/VI-writable-custom-stream/writeToFIle.js
@@ -8,13 +8,13 @@ const tfs = new ToFileStream({objectMode: true});
 // We cae see that the input of our stream are objects
 // and not strings or buffers;
 // this means that our stream has to work in oject mode
-tfs.write.call(tfs, {path: "./test/file1.txt", content: "Hello"}, 'utf8', ()=>{
+tfs.write.call(tfs, {path: "./test/file1.txt", content: "Hello"}, 'utf8', () => {
     console.log('file1 done');
 });
-tfs.write.call(tfs, {path: "./test/file2.txt", content: "Node.js"}, 'utf8', ()=>{
+tfs.write.call(tfs, {path: "./test/file2.txt", content: "Node.js"}, 'utf8', () => {
     console.log('file2 done');
 });
-tfs.write.call(tfs, {path: "./test/file3.txt", content: "Stream"}, 'utf8', ()=>{
+tfs.write.call(tfs, {path: "./test/file3.txt", content: "Stream"}, 'utf8', () => {
     console.log('file3 done');
 });
 

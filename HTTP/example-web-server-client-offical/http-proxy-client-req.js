aff3dc94728bf3fd3a2668b72d8557c5d81f1f71
Calvin Jin <jinchi2013us@gmail.com>
2017-03-05 16:49:35 -0500

    Space

--- HTTP/example-web-server-client-offical/http-proxy-client-req.js
+++ HTTP/example-web-server-client-offical/http-proxy-client-req.js
@@ -11,16 +11,17 @@ var proxy = http.createServer( (req, res)=>{
 proxy.on('connect', (req, cltSocket, head)=>{
 	//connect to an origin server
 	var srvUrl = url.parse(`http://${req.url}`);
-	var srvSocket = net.connect(srvUrl.port, 
-								srvUrl.hostname, 
-								()=>{
-									cltSocket.write('HTTP/1.1 200 Connection Established\r\n' + 
-													'Proxy-agent: Node.js-Proxy\r\n' + 
-													'\r\n');
-									srvSocket.write(head);
-									srvSocket.pipe(cltSocket);
-									srvSocket.pipe(srvSocket);
-								});
+	var srvSocket = 
+		net.connect(srvUrl.port, 
+					srvUrl.hostname, 
+					()=>{
+						cltSocket.write('HTTP/1.1 200 Connection Established\r\n' + 
+										'Proxy-agent: Node.js-Proxy\r\n' + 
+										'\r\n');
+						srvSocket.write(head);
+						srvSocket.pipe(cltSocket);
+						srvSocket.pipe(srvSocket);
+					});
 
 });
 

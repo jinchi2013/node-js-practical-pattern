a5ff35ceac19e855c186da64c8800811bdac5c4e
Chi Jin <jinchi2013us@gmail.com>
2017-03-22 23:07:53 -0400

    Main app

---
+++ VI-Recipes/running-CPU-bound-tasks/app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+// var Subsetsum = require('./Subsetsum');
+// var Subsetsum = require('./subsetSumDefer');
+var SubsetSum = require('./processPool/subsetForkSum')
+
+http.createServer(function(req, res){
+	var url = require('url').parse(req.url, true);
+
+	if(url.pathname === '/subsetSum') {
+		var data = JSON.parse(url.query.data);
+		res.writeHead(200);
+
+		var subsetSum = new SubsetSum(url.query.sum, data);
+
+		subsetSum.on('match', function(match) {
+			res.write(`Match: ${JSON.stringify(match)} \n`);
+		});
+
+		subsetSum.on('end', function(){
+			res.end();
+		});
+
+		subsetSum.start();
+	} else {
+		res.writeHead(200);
+		res.end('I\m alive!\n');
+	}
+}).listen(8000, function() {console.log('Started!')});

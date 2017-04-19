f8dfebcd751edd401b5e8b5df858b59134a3a613
Calvin Jin <jinchi2013us@gmail.com>
2017-04-19 12:40:24 -0400

    type mutation and input

--- Rest_VS_GraphQl/Express_GraphQL_Server/index.js
+++ Rest_VS_GraphQl/Express_GraphQL_Server/index.js
@@ -4,38 +4,99 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+	input MessageInput {
+		content: String
+		author: String
+	}
+
+	type Message {
+		id: ID!
+		content: String
+		author: String
+	}
+
+	type Mutation {
+		createMessage(input: MessageInput): Message
+		updateMessage(id: ID!, input: MessageInput): Message
+	}
+
+	type RandomDie {
+		numSides: Int!
+		rollOnce: Int!
+		roll(numRolls: Int!): [Int]
+	}
+
 	type Query {
-		hello: String
-		quoteOfTheDay: String
-		random: Float!
-		rollThreeDice: [Int]
 		rollDice(numDice: Int!, numSides: Int): [Int]
+		getDie(numSides: Int): RandomDie
+		getMessage(id: ID!): Message
 	}
 `);
 
 // Use an exclamation point to indicate a type cannot be nullable, so String! is a non-nullable string
 // To use a list type, surround the type in square brackets, so [Int] is a list of integers
 
+class RandomDie {
+	constructor(numSides) {
+		this.numSides = numSides;
+	}
+
+	rollOnce() {
+		return 1 + Math.floor(Math.random() * this.numSides);
+	}
+
+	roll({numRolls}) {
+		var output = [];
+		for(var i = 0; i < numRolls; i++) {
+			output.push(this.rollOnce())
+		}
+		return output;
+	}
+}
+
+const Message = (id, {content, author}) => ({
+	id: id,
+	content: content,
+	author: author
+})
+
 // The root provides a resolver function for each API endpoint
+// When a resolver takes arguments, they are passed as one “args” object, 
+// as the first argument to the function.
+
+var fakeDatabase = {};
+
 var root = {
-	hello: () => {
-		return 'Hello World!';
-	},
-	quoteOfTheDay: () => {
-		return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-	},
-	random: ()=>{
-		return Math.random();
-	},
-	rollThreeDice: () => {
-		return [1,2,3].map( num => 1 + Math.floor(Math.random() * 6))
-	},
 	rollDice: function({numDice, numSides}) {
 		var output = [];
 		for(var i = 0; i< numDice; i++) {
 			output.push(1 + Math.floor(Math.random() * (numSides || 6)));
 		}
 		return output;
+	},
+	getDie: function({ numSides }) {
+		return new RandomDie(numSides || 6)
+	},
+	getMessage: function({id}) {
+		if(!fakeDatabase[id]) {
+			throw new Error(`no message exists with id ${id}`);
+		}
+		return Message(id, fakeDatabase[id])
+	},
+	createMessage: function({input}) {
+		// Create a random id for our "database"
+		let id = require('crypto').randomBytes(10).toString('hex');
+
+		fakeDatabase[id] = input;
+		return Message(id, fakeDatabase[id])
+	},
+	updateMessage: function({id, input}) {
+		if(!fakeDatabase[id]) {
+			throw new Error(`no message exists with id ${id}`);
+		}
+		// This replaces all old data, but some ap
+		fakeDatabase[id] = Object.assign({},fakeDatabase[id], input);
+		return Message(id, fakeDatabase[id])
 	}
 };
 
